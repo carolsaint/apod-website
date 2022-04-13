bb3e36cd7310e45f42412350af87e65649c93f35
carolsaint <santocarolina98@gmail.com>
2022-04-12 22:57:31 -0300

    js started, css done in blocks

--- codigos.js
+++ codigos.js
@@ -1,7 +1,17 @@
-$.ajax{
-    url: `    `;
-    success: function(resposta){
-        div.append(<img></img>);
-        
-    }
-}
+$ ("#input").on("change", function (event){
+    event.preventDefault();
+
+    let input = $("#input")
+    let img = $("#imgOut")
+    let texto = $("#texto")
+
+    $.ajax({
+        url: `https://api.nasa.gov/planetary/apod?api_key=bZD0thHorWv70ZZhFwnL0MajObm3gQEy0euFry4s&date=${input.val()}`,
+        success: function(resposta){
+
+            img.attr("src", resposta.url),
+            texto.text(resposta.explanation)
+
+        }
+    });
+});
