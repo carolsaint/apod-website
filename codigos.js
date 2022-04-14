90ec6dd98d92d39866d308fb9897a6dec5891376
carolsaint <santocarolina98@gmail.com>
2022-04-14 20:09:51 -0300

    date validation and github  link added

--- codigos.js
+++ codigos.js
@@ -16,6 +16,7 @@ $ ("#sub").on("click", function (event){
     $.ajax({
         url: `https://api.nasa.gov/planetary/apod?api_key=bZD0thHorWv70ZZhFwnL0MajObm3gQEy0euFry4s&date=${input.val()}`,
         success: function(resposta){
+            $("#input").attr("max", resposta.date)
             if (resposta.media_type == "image") {
                 if ($("#imgOut").show()) {
                     $("videoOut").hide()
@@ -29,5 +30,6 @@ $ ("#sub").on("click", function (event){
         }
         texto.text(resposta.explanation).show()
     },
+
 });
 })
