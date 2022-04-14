2f8629373b03f908bd64e800d7ee38d4d2bbb9a1
carolsaint <santocarolina98@gmail.com>
2022-04-14 19:40:04 -0300

    style changed, bg added, hover added

--- codigos.js
+++ codigos.js
@@ -1,22 +1,33 @@
 $("#imgOut").hide()
+$("#videoOut").hide()
+$("#texto").hide()
 
-$ ("#input").on("change", function (event){
+
+$ ("#sub").on("click", function (event){
     event.preventDefault();
 
     let input = $("#input")
     let img = $("#imgOut")
     $("#imgOut").show()
     let texto = $("#texto")
+    let video = $("#videoOut")
 
 
     $.ajax({
         url: `https://api.nasa.gov/planetary/apod?api_key=bZD0thHorWv70ZZhFwnL0MajObm3gQEy0euFry4s&date=${input.val()}`,
         success: function(resposta){
+            if (resposta.media_type == "image") {
+                if ($("#imgOut").show()) {
+                    $("videoOut").hide()
+                }
+                $("#imgOut").attr("src", resposta.url)
 
-            img.attr("src", resposta.url),
-            texto.text(resposta.explanation)
-
+            } else if (resposta.media_type == "video") {
+                  ($("#videoOut").show()); 
+                   $("#imgOut").hide()
+                $("#videoOut").attr("src", resposta.url)
         }
-    });
+        texto.text(resposta.explanation).show()
+    },
 });
-
+})
